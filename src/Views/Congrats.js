2d6299a07dd9d0c6e476820f58718303e3172733
Lucas Lacava <lucas.lacava@ab-inbev.com>
2020-09-01 13:37:11 -0300

    Congrats component and unit tests

---
+++ src/Views/Congrats.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Proptypes from "prop-types";
+/**
+ * Functional react component for congrats message
+ * @function
+ * @param {object} props - React props
+ * @return {JSX.Element} = Rendered component or null
+ */
+const Congrats = ({ success }) => {
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
+
+Congrats.propTypes = {
+  success: Proptypes.bool.isRequired,
+};
+
+export default Congrats;
