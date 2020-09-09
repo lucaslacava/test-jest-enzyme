37431ebcb981da189ddc81e4bd800f44aa68247c
Lucas Lacava <lucas.lacava@ab-inbev.com>
2020-09-09 10:39:51 -0300

    add bootstrap styling and matchCountLetters function

--- src/Views/GuessedWords.js
+++ src/Views/GuessedWords.js
@@ -7,6 +7,27 @@ const GuessedWords = (props) => {
     content = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed words</h3>
+        <table className="table table-sm">
+          <thead className="thead-light">
+            <tr>
+              <th>Guess</th>
+              <th>Matching letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{content}</div>;
 };
